2b054dba17b2a0cb9752f898f7965c536058b21f
SuvorovaSvetlana <sashazhitnikdr.@gmail.com>
2022-12-23 20:16:49 +0300

    add firebase config

--- src/services/Cloud.js
+++ src/services/Cloud.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 export class CloudService {
   constructor() {
     this.config = {
-      apiKey: process.env.API_KEY,
+      apiKey: "AIzaSyADGuzPSHmwmRrN5c8btMUKOsaYAAcFdQ0",
       authDomain: "floristman-a4faf.firebaseapp.com",
       projectId: "floristman-a4faf",
       storageBucket: "floristman-a4faf.appspot.com",
       messagingSenderId: "282969411711",
-      appId: process.env.APP_ID
+      appId: "1:282969411711:web:d132b449040d7f34676f4a"
     };
 
     this.app = initializeApp(this.config);
